baee655b5edd7a4e451eabe2c88dd9e887fe19df
leo-prata <leo.prata@estudante.ufjf.br>
2025-05-15 13:49:25 -0300

    basic test examples

--- src/app/Utils.ts
+++ src/app/Utils.ts
@@ -1,3 +1,21 @@
 export function toUpperCase(arg: string) {
 	return arg.toUpperCase();
 }
+
+export type stringInfo = {
+	lowerCase: string;
+	upperCase: string;
+	characters: string[];
+	lenght: number;
+	extraInfo: Object | undefined;
+};
+
+export function getStringInfo(arg: string): stringInfo {
+	return {
+		lowerCase: arg.toLowerCase(),
+		upperCase: arg.toUpperCase(),
+		characters: Array.from(arg),
+		lenght: arg.length,
+		extraInfo: {},
+	};
+}
--- src/tests/Utils.test.ts
+++ src/tests/Utils.test.ts
@@ -1,8 +1,45 @@
-import { toUpperCase } from '../app/Utils';
+import { getStringInfo, toUpperCase } from '../app/Utils';
 
 describe('utils test suite', () => {
 	it('should return uppercase string', () => {
-		const result = toUpperCase('testing');
-		expect(result).toBe('TESTING');
+		//arrange
+		const sut = toUpperCase;
+		const expected = 'TESTING';
+
+		//act
+		const actual = sut('testing');
+
+		//assert
+		expect(actual).toBe(expected);
+	});
+
+	describe('ToUpperCase examples', () => {
+		it.each([
+			{ input: 'abc', expected: 'ABC' },
+			{ input: 'test-teste', expected: 'TEST-TESTE' },
+			{ input: 'jpeg', expected: 'JPEG' },
+		])('$input toUpperCase should be $expected', ({ input, expected }) => {
+			const actual = toUpperCase(input);
+			expect(actual).toBe(expected);
+		});
+	});
+
+	describe('getStringInfo for arg My-String should', () => {
+		test('return right lenght', () => {
+			const actual = getStringInfo('My-String');
+			expect(actual.characters).toHaveLength(9);
+		});
+		test('return lowercase', () => {
+			const actual = getStringInfo('My-String');
+			expect(actual.lowerCase).toBe('my-string');
+		});
+		test('return defined extra info', () => {
+			const actual = getStringInfo('My-String');
+			expect(actual.extraInfo).toBeDefined();
+		});
+		test('return right extra info', () => {
+			const actual = getStringInfo('My-String');
+			expect(actual.extraInfo).toEqual({});
+		});
 	});
 });
